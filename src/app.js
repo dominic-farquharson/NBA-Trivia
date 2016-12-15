75c79d4d07b5a2d872d6d2bad29b47cdcbc94e67
Dominic Farquharson <farquharsondominic@gmail.com>
2016-12-15 10:22:13 -0500

    Updated Player Class

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 //Player Class
 class Player {
-  constructor(timer, questions) {
+  constructor(timer, questions, attempts, input) {
     this.timer = timer;
     this.questions = questions;
+    this.attempts = attempts;
+    this.input = input;
   }
   //Methods
   //Decrease the timer
@@ -14,14 +16,43 @@ class Player {
   timerIncrease() {
     this.timer +=5;
   }
-
+  //Check if Question is right or wrong
+  checkRight() {
+    check(this.input);
+  }
 
 }
+//Creating Player 1
+let Player1 = new Player(60,'hello', 2, 3);
+//Grabbing Answer Choice Inputs
+$('.one').click(Player1.checkRight);
+
+  //Player.checkRight);
+
+
+
+
+//Checks for write or wrong answer
+function check(x) {
+  console.log('I hear you.');
+  if(this.input === 3) {
+    console.log('you are correct');
+  }
+  else {
+    Player1.timerDecrease;
+    console.log(Player1.timer);
+  }
+}
+
+//Questions class?
 
 //testing
-let Player1 = new Player(60, 'hello');
+
 console.log(Player1.timer);
 console.log(Player1);
 Player1.timerDecrease();
 console.log(Player1.timer);
 console.log(Player1);
+Player1.timerIncrease();
+Player1.timerIncrease();
+console.log(Player1.timer);
