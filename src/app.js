b3b56f6cdc96bc0bd8d1612c2d58ebcf354250e8
Dominic Farquharson <Dominic Farquharson>
2016-12-18 18:14:45 -0500

    Added Computer. Updated Style sheet

--- src/app.js
+++ src/app.js
@@ -14,49 +14,30 @@ let Player1 = new Player(0, 59);
 //Creating computer
 let Computer1 = new Computer(0, 15);
 
-/*//timer
-function counter() {
-  //Interval runs every second
-  //let counters = 60;
-  let timer = setInterval(function() {
-  $('#timer').html(Player1.time + 's');
-    Player1.time--;
-    if(Player1.time === -1) {
-      // alert('time is up');
-      clearInterval(timer);
-      //End game when time is up
-      alert('Time is up!');
-      endStartNew();
-    }
-
-  }, 1000);
-}
-*/
-
-
-
 //End Game Screen - Restart Game
 function endStartNew() {
-    //$('#options').html(Player1.score);
-    //Hiding Quiz Questions + Timer
+    //Hiding Quiz Questions + Timer,
     $('#options').velocity('slideUp', {duration:800});
     $('#question').velocity('slideUp', {duration:800});
     $('#timer').velocity('slideUp', {duration:800});
     $('#clock>h3').velocity('slideUp', {duration:800});
-
-
-
-
+    //making try again button appear
+    $('#tryAgain').toggle();
+    if(Player1.score>Computer1.score) {
+      alert('You have defeated the Comptuer');
+    }
+    if(Player1.score==Computer1.score) {
+      alert('You have tied the Comptuer');
+    }
+    else if(Player1.score<Computer1.score) {
+      alert('The computer has beaten you!')
+    }
 
 }
 
 
-
-
-//Outputting Counter
-// counter();
 //printing Question 1
 printQA(0);
-//Updating Score
+//Outputting Initial Score
 $('#playerScore').html(Player1.score);
 $('#computerScore').html(Computer1.score);
--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,18 +1,11 @@
 console.log('gameLogic is connected');
-let x = 0;
-// function print(y) {
-//     console.log(Questions[y])
-// }
-
 //Grabbing innerHTML of question choices to compare with correct answer
 $(`.option0`).click(function(){checkRight(this.innerHTML)});
 $(`.option1`).click(function(){checkRight(this.innerHTML)});
 $(`.option2`).click(function(){checkRight(this.innerHTML)});
 $(`.option3`).click(function(){checkRight(this.innerHTML)});
-
-
-
 //printing questions and answer
+let x = 0;
 function printQA(x) {
     //using Velocity to add a fade effect
     $('#question').text(Questions[x].question).velocity('fadeIn', {duration:200});
@@ -21,41 +14,38 @@ function printQA(x) {
   }
 }
 
-
 //index will represent question number, using it to change questions after correct answer selection
 let index=0;
 //Check if Choice is correct answer
 function checkRight(userChoice) {
-//Running computer score random number
-Computer1.checkRight();
-//UPdating Computer's score
-$('#computerScore').text(Computer1.score);
+
 
   if(userChoice === Questions[index].correctAnswer){
-    //changing question 1
+    //changing question
     index++;
     //ending game after last question
-    if(index==Questions.length){
-
+    if(index===Questions.length){
       endStartNew();
-
     }
     //updating score if correct
     Player1.updateScore();
       $('#playerScore').text(Player1.score);
 
-    //Alert Correct
-    // alert('Correct!');
     //changing Question
     printQA(index)}
     else {
 
     //change Questions
       index++;
-      if(index>4){
+      if(index===Questions.length){
         endStartNew();
       }
+      //changing question
       printQA(index);
     }
+    //Running computer score random number
+    Computer1.checkRight();
+    //UPdating Computer's score
+    $('#computerScore').text(Computer1.score);
 
   }
